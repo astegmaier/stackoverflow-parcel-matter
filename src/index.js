770d46eb44c7ea1989cc1f3f769ab4fceb4d15a5
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2022-05-19 10:17:07 -0400

    Import matter and poly-decomp.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hello world!")
+import Matter from "matter-js"
+console.log("here is matter: ", Matter)
+
+import PolyDecomp from "poly-decomp";
+console.log("here is poly-decomp", PolyDecomp)
